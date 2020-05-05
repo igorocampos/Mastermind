44251892400592a1a6f4b48e7fc54321c6a08e06
Igor Campos <igor.campos@wexinc.com>
2020-05-04 23:09:27 -0300

    Added frmMenu

---
+++ Mastermind/Mastermind/frmMenu.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Mastermind
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSinglePlayer = new System.Windows.Forms.Button();
+            this.btnTwoPlayers = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSinglePlayer
+            // 
+            this.btnSinglePlayer.Location = new System.Drawing.Point(38, 105);
+            this.btnSinglePlayer.Name = "btnSinglePlayer";
+            this.btnSinglePlayer.Size = new System.Drawing.Size(225, 44);
+            this.btnSinglePlayer.TabIndex = 0;
+            this.btnSinglePlayer.Text = "SINGLE PLAYER";
+            this.btnSinglePlayer.UseVisualStyleBackColor = true;
+            this.btnSinglePlayer.Click += new System.EventHandler(this.btnSinglePlayer_Click);
+            // 
+            // btnTwoPlayers
+            // 
+            this.btnTwoPlayers.Location = new System.Drawing.Point(38, 155);
+            this.btnTwoPlayers.Name = "btnTwoPlayers";
+            this.btnTwoPlayers.Size = new System.Drawing.Size(225, 44);
+            this.btnTwoPlayers.TabIndex = 1;
+            this.btnTwoPlayers.Text = "TWO PLAYERS";
+            this.btnTwoPlayers.UseVisualStyleBackColor = true;
+            this.btnTwoPlayers.Click += new System.EventHandler(this.btnTwoPlayers_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(38, 205);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(225, 44);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "QUIT";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 354);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnTwoPlayers);
+            this.Controls.Add(this.btnSinglePlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSinglePlayer;
+        private System.Windows.Forms.Button btnTwoPlayers;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
---
+++ Mastermind/Mastermind/frmMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Mastermind
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSinglePlayer_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            this.Hide();
+
+            using (var form = new frmMain((eColor)rnd.Next(0, 5), (eColor)rnd.Next(0, 5), (eColor)rnd.Next(0, 5), (eColor)rnd.Next(0, 5)))
+            {
+                form.ShowDialog();
+                if (form.CloseAll)
+                    this.Close();
+                else
+                    this.Show();
+            }
+        }
+
+        private void btnTwoPlayers_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            using (var form = new frmChooseAnswer())
+            {
+                form.ShowDialog();
+                if (form.CloseAll)
+                    this.Close();
+                else
+                    this.Show();
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
