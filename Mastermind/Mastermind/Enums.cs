5996565b77296bef6691e0aff3c4d81e51c71607
Igor Campos <igor.campos@wexinc.com>
2020-05-04 22:14:38 -0300

    Added enum

---
+++ Mastermind/Mastermind/Enums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace Mastermind
+{
+    public enum eColor
+    {
+        White,
+        Green,
+        Blue,
+        Red,
+        Yellow,
+        Purple,
+        Pink
+    }
+
+    public static class Enums
+    {
+        public static Color ColorFromEnum(eColor color)
+        {
+            switch (color)
+            {
+                case eColor.White:  return Color.White;
+                case eColor.Green:  return Color.Green;
+                case eColor.Blue:   return Color.Blue;
+                case eColor.Red:    return Color.Red;
+                case eColor.Yellow: return Color.Yellow;
+                case eColor.Purple: return Color.Purple;
+                case eColor.Pink:   return Color.Pink;
+                default: throw new ArgumentException("Invalid color code.", nameof(color));
+            }
+        }
+    }
+}
